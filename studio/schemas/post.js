198fc10e32cb5776e2513069fd2889f957f5c12d
alfathmas <muhammad.ayub412@gmail.com>
2021-04-30 06:50:52 +0700

    add blockContent

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -1,65 +1,68 @@
-export default {
-  name: 'post',
-  title: 'Post',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string'
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96
-      }
-    },
-    {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'}
-    },
-    {
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true
-      }
-    },
-    {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}]
-    },
-    {
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime'
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent'
-    }
-  ],
+import Default from "part:@sanity/form-builder/input/image/asset-source-default";
+import Cloudinary from "part:sanity-plugin-asset-source-cloudinary/image-asset-source";
 
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage'
+export default {
+    name: "post",
+    title: "Post",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            title: "Title",
+            type: "string",
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "title",
+                maxLength: 96,
+            },
+        },
+        {
+            name: "author",
+            title: "Author",
+            type: "reference",
+            to: { type: "author" },
+        },
+        {
+            name: "mainImage",
+            title: "Main image",
+            type: "image",
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: "categories",
+            title: "Categories",
+            type: "array",
+            of: [{ type: "reference", to: { type: "category" } }],
+        },
+        {
+            name: "publishedAt",
+            title: "Published at",
+            type: "datetime",
+        },
+        {
+            name: "body",
+            title: "Body",
+            type: "blockContent",
+        },
+    ],
+
+    preview: {
+        select: {
+            title: "title",
+            author: "author.name",
+            media: "mainImage",
+        },
+        prepare(selection) {
+            const { author } = selection;
+            return Object.assign({}, selection, {
+                subtitle: author && `by ${author}`,
+            });
+        },
     },
-    prepare(selection) {
-      const {author} = selection
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`
-      })
-    }
-  }
-}
+};
